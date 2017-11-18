93071f4a1ea85b7c956e05b83432f2532d52425d
AkshayAyyanchira <aayyanch@uncc.edu>
2017-11-17 22:26:54 -0500

    Trying second message

--- Inbound.js
+++ Inbound.js
@@ -1,3 +1,11 @@
+var accountSid = 'AC951998358fcf4cddaaa0c620ef5b6a94'; // Your Account SID from www.twilio.com/console
+var authToken = '0936ea65648cfe0b9a5bcc78418b2ac1';   // Your Auth Token from www.twilio.com/console
+var twilio = require('twilio');
+var client = new twilio(accountSid, authToken);
+
+
+
+
 const http = require('http');
 const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
@@ -16,11 +24,9 @@ if (request.body.Body == "STARTAPP" && studyEnrolled == false){
     var started = "Welcome to the study."
     sendMessage(started,response);
     var symptomQuestion = "Please indicate your symptom (1)Headache, (2)Dizziness, (3)Nausea, (4)Fatigue, (5)Sadness, (0)None";
-    sendMessageWithoutHeader(symptomQuestion,response);
+    sendMessageWithoutHeader(request.body.From,symptomQuestion,response);
 }
 
-
-  
 });
 
 
@@ -32,11 +38,17 @@ function sendMessage(messageBody,responseObject){
     //responseObject.end(twiml.toString());
 }
 
-function sendMessageWithoutHeader(messageBody,responseObject){
+function sendMessageWithoutHeader(from,messageBody,responseObject){
     console.log('Send message is being called');
     const twiml = new MessagingResponse();
     twiml.message(messageBody);
     responseObject.end(twiml.toString());
+    client.messages.create({
+        body: messageBody,
+        to: from,  // Text this number
+        from: '+18642077505' // From a valid Twilio number
+    })
+    .then((message) => console.log(message.sid));
 }
 
 http.createServer(app).listen(1337, () => {

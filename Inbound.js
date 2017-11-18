9010fc58a5c48be4964b6ece6923a81da5ebcbd5
AkshayAyyanchira <aayyanch@uncc.edu>
2017-11-17 21:21:33 -0500

    another attempt

--- Inbound.js
+++ Inbound.js
@@ -15,6 +15,22 @@ app.post('/sms', (req, res) => {
   res.end(twiml.toString());
 });
 
+function censor(censor) {
+    var i = 0;
+  
+    return function(key, value) {
+      if(i !== 0 && typeof(censor) === 'object' && typeof(value) == 'object' && censor == value) 
+        return '[Circular]'; 
+  
+      if(i >= 29) // seems to be a harded maximum of 30 serialized objects?
+        return '[Unknown]';
+  
+      ++i; // so we know we aren't using the original object anymore
+  
+      return value;  
+    }
+  }
+
 http.createServer(app).listen(1337, () => {
   console.log('Express server listening on port 1337');
 });

cc25edf07dcce2301cec7521f61aeb7a387792a6
AkshayAyyanchira <aayyanch@uncc.edu>
2017-11-17 20:54:54 -0500

    another attempt

--- Inbound.js
+++ Inbound.js
@@ -7,7 +7,7 @@ const app = express();
 app.post('/sms', (req, res) => {
     const twiml = new MessagingResponse();
     var msg = req.message;
-    console.log('fetched message by my server with '+msg + req);
+    console.log('fetched message by my server with '+msg + req.body);
     twiml.message('The Robots are coming! Head for the hills!');
 //   twiml.message('The Robots are coming! Head for the hills!');
 

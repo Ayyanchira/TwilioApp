65447a800d692bcf965d56be6badc81f9d06ad63
AkshayAyyanchira <aayyanch@uncc.edu>
2017-11-17 21:25:25 -0500

    Another Attempt

--- Inbound.js
+++ Inbound.js
@@ -4,32 +4,15 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 const app = express();
 
-app.post('/sms', (req, res) => {
+app.post('/sms', (request, response) => {
     const twiml = new MessagingResponse();
-    console.log(JSON.stringify(req,censor(req)));
-    console.log('fetched message by my server with '+req);
+    // console.log(JSON.stringify(request,censor(req)));
+    console.log('fetched message by my server with '+request);
     twiml.message('The Robots are coming! Head for the hills!');
-//   twiml.message('The Robots are coming! Head for the hills!');
 
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
 });
-
-function censor(censor) {
-    var i = 0;
-  
-    return function(key, value) {
-      if(i !== 0 && typeof(censor) === 'object' && typeof(value) == 'object' && censor == value) 
-        return '[Circular]'; 
-  
-      if(i >= 29) // seems to be a harded maximum of 30 serialized objects?
-        return '[Unknown]';
-  
-      ++i; // so we know we aren't using the original object anymore
-  
-      return value;  
-    }
-  }
 
 http.createServer(app).listen(1337, () => {
   console.log('Express server listening on port 1337');
